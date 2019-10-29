7ebd1395d87bb980772aaecbbc1c4840357e1e10
alex-bellon <alexrbellon@gmail.com>
2019-10-29 15:14:46 -0500

    Moved code to vim.js, added script to all pages

--- assets/js/vim.js
+++ assets/js/vim.js
@@ -1,20 +1,19 @@
 var pos = 0;
-
-function nav_hotkey(e) {
-  var action_for_key = function(e) {
-    switch (e.keyCode) {
-      case 74:  // 'j'
-        return 'down';
-      case 75:  // 'k'
-        return 'up';
+var jump = 250;
+document.onkeydown = function(e) {
+  e = e || window.event;
+  if (e.keyCode == 74){ //j
+    if (pos + jump <= 2000){ // Fix this. If you try to do scrollHeight, you need to make sure it calculates that after everything loads.
+      pos += jump;
     }
-  }
-  var action = action_for_key(e);
-  if (action == 'down') {
-    pos += 300;
-    window.scrollTo(0, pos);
-  } else if (action == 'up') {
-    pos -= 300;
+    console.log(pos);
     window.scrollTo(0, pos);
   }
-}
+  else if (e.keyCode == 75){ //k
+    if (pos - jump >= 0){
+      pos -= jump;
+    }
+    console.log(pos);
+    window.scrollTo(0, pos);
+  }
+};
