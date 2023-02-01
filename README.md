b89f4e5f017302452256ff986b57f06d39e69794
Alex Bellon <abellon@ucsd.edu>
2023-02-01 14:52:25 -0800

    get rid of last updated script

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Website
-
-Website last updated on: Wed Feb 1 2023 12:41:24
-    
+ 
 Source code for my personal website <a href="http://alex-bellon.com">alex-bellon.com</a>. I wrote everything on the website myself.
--- update-readme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-while inotifywait -e modify -e create /home/alex/GitHub/website; do
-	echo "# Website
-
-Website last updated on: $(date)
-    
-Source code for my personal website <a href=\"http://alex-bellon.com\">alex-bellon.com</a>. I wrote the website myself except for the <a href=\"https://github.com/jsoma/tabletop\">tabletop</a> (allows HTML content to be drawn from a Google Spreadsheet)." > README.md
-done
